403ea9f423cedbe34e690da72b1e7482d8f9a07e
Nat Grant <email.natgrant@gmail.com>
2019-02-17 10:42:11 +1300

    updating lodash

--- client/reducers/active_book.js
+++ client/reducers/active_book.js
@@ -1,5 +1,7 @@
 // state argument is not app state, only state this reducer is responsible for
 
+const initialState = []
+
 export default function(state = null, action) {
   switch (action.type) {
     case "BOOK_SELECTED":
