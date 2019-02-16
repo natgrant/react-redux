d14c7e6a54d763e85f8f356e629c38843dc92c75
Nat Grant <email.natgrant@gmail.com>
2019-02-16 15:47:17 +1300

    view books and pages working

--- client/containers/BookList.jsx
+++ client/containers/BookList.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
+// actions need to bind
+import { selectBook } from "../actions/index";
+import { bindActionCreators } from "redux";
+
 class BookList extends Component {
   renderList() {
     return this.props.books.map(book => {
       return (
-        <li key={book.title} className="list-group-item">
+        <li
+          key={book.title}
+          onClick={() => this.props.selectBook(book)}
+          className="list-group-item"
+        >
           {book.title}
         </li>
       );
@@ -18,13 +26,24 @@ class BookList extends Component {
 
 // Take applications state as an argument
 function mapStateToProps(state) {
-  // Whatever is returned will show up as props inside BookList
+  // Whatever is returned will show up as props inside BookList!
+  // This is the glue ties state to props
+  // if app state changes this component BookList will auto re-render
   return {
     books: state.books
   };
 }
 
-export default connect(mapStateToProps)(BookList);
+// Anything returned from this function will end up as props on the Booklist container
+function mapDispatchToProps(dispatch) {
+  // Whenever selectbook is called the result should be passed to all of our reducers
+  return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(BookList);
 
 // Containers are components which can connect React & Redux (Smart component)
 // BookList cares about when the list of books changes
--- client/reducers/reducer_books.js
+++ client/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: "Javascript: The Good Parts" },
-    { title: "Harry Potter" },
-    { title: "The Dark Tower" },
-    { title: "Eloquent Rubyist" }
+    { title: "Javascript: The Good Parts", pages: 90 },
+    { title: "Harry Potter", pages: 45 },
+    { title: "The Dark Tower", pages: 890 },
+    { title: "Eloquent Rubyist", pages: 9 }
   ];
 }
